32aae769a9a3cb98f166afd0bb9d388c4491209a
Akonaay <ambrosesulley4@gmail.com>
2021-08-27 12:34:41 +0300

    UI commit

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+  $(document).ready(function () {
+    $("#owl-agency").owlCarousel({
+      loop: true,
+      nav: false,
+      autoplay: true,
+      margin: 50,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        736: {
+          items: 1,
+          nav: false
+        }
+      }
+    })
+
+    //  Mission/Vision Accordion
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("pactive");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  })
